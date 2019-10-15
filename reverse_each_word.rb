421b4d513f44ad0bff11b8324fce3599782ae8ca
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 04:21:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  sentence_array.reverse_each
-  sentence_array.join(" ")
+  reversed_array = sentence_array.reverse_each
+  reversed_array.join
+  
 end
